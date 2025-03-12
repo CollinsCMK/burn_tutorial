0c0fde1f531c0abd4711c593352305115ef267c0
CollinsCMK <collinscmkdev@gmail.com>
2025-03-12 06:40:42 +0300

    CMK: Added inference.rs performs inference on an MNIST image

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use burn::backend::Wgpu;
+use burn::{backend::{Autodiff, Wgpu}, data::dataset::Dataset, optim::AdamConfig};
 use model::ModelConfig;
+use training::TrainingConfig;
 
 mod data;
 mod model;
 mod training;
+mod inference;
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
-
-    println!("{}", model);
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "/tmp/guide";
+    crate::training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
+    crate::inference::infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test()
+            .get(12)
+            .unwrap(),
+    );
 }
