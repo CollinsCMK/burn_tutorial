18108550660ddede6474c226d467cd4ae42875e4
CollinsCMK <collinscmkdev@gmail.com>
2025-03-12 06:51:02 +0300

    fix: to run command for train or infer - CMK

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use burn::{backend::{Autodiff, Wgpu}, data::dataset::Dataset, optim::AdamConfig};
+use std::env;
+
 use model::ModelConfig;
 use training::TrainingConfig;
 
@@ -13,16 +15,38 @@ fn main() {
 
     let device = burn::backend::wgpu::WgpuDevice::default();
     let artifact_dir = "/tmp/guide";
-    crate::training::train::<MyAutodiffBackend>(
-        artifact_dir,
-        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
-        device.clone(),
-    );
-    crate::inference::infer::<MyBackend>(
-        artifact_dir,
-        device,
-        burn::data::dataset::vision::MnistDataset::test()
-            .get(12)
-            .unwrap(),
-    );
+
+    // Get command-line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: cargo run --release train | infer");
+        return;
+    }
+
+    match args[1].as_str() {
+        "train" => {
+            println!("Starting Training...");
+            crate::training::train::<MyAutodiffBackend>(
+                artifact_dir,
+                TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+                device.clone(),
+            );
+            println!("Training Complete!");
+        }
+        "infer" => {
+            println!("Running Inference...");
+            let mnist_sample = burn::data::dataset::vision::MnistDataset::test()
+                .get(12)
+                .unwrap();
+
+            crate::inference::infer::<MyBackend>(
+                artifact_dir,
+                device,
+                mnist_sample,
+            );
+        }
+        _ => {
+            println!("Invalid command! Use 'train' or 'infer'.");
+        }
+    }
 }
